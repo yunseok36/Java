7a0cb7ee3ea199cf65d22793ae1558d46c1f3e1b
yunseok36 <kys010306@gmail.com>
2025-02-09 21:24:56 +0900

    Add files via upload

---
+++ Korean.java
@@ -0,0 +1,13 @@
+package sec03.exam02;
+
+public class Korean {
+	
+	String nation = "대한민국"; // 필드 선언
+	String name;
+	String ssn;
+	
+	public Korean(String n, String s) { // 생성자 선언
+		name = n;
+		ssn = s;
+	}
+}
