dd5a9e27b220e7bf99ecf4a7ba90c5e9c749b126
yunseok36 <kys010306@gmail.com>
2025-02-18 20:03:54 +0900

    Add files via upload

---
+++ Calculator2Example.java
@@ -0,0 +1,8 @@
+package sec06.exam01;
+
+public class Calculator2Example {
+	public static void main(String[] args) {
+	Calculator2 myCalc = new Calculator2();
+	myCalc.execute(); // Calculator execute() 메소드 호출
+	}
+}
