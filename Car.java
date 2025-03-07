1f38da38f05008937b65741a3ad5d14c63dcfa97
yunseok36 <kys010306@gmail.com>
2025-03-07 15:22:16 +0900

    Add files via upload

--- Car.java
+++ Car.java
@@ -1,16 +1,25 @@
-package sec07.exam01;
+package sec07.exam02;
 
 public class Car {
 
-	// 필드 선언
-	public int speed;
+	// 필드 선언'
+	Tire frontLeftTire = new Tire("앞왼쪽", 6);
+	Tire frontRightTire = new Tire("앞오른쪽", 2);
+	Tire backLeftTire = new Tire("뒤왼쪽", 3);
+	Tire backRightTire = new Tire("뒤오른쪽", 4);
+	
+	// 생성자
 	
 	// 메소드
-	public void speedUp() { speed += 1; } // speed = speed + 1
+	int run() {
+		System.out.println("[자동차가 달립니다.]");
+		if(frontLeftTire.roll() == false) { stop(); return 1; }
+		if(frontRightTire.roll() == false) { stop(); return 2; }
+		if(backLeftTire.roll() == false) { stop(); return 3; }
+		if(backRightTire.roll() == false) { stop(); return 4; }
+	}
 	
-	// final 메소드
-	public final void stop() {
-		System.out.println("차를 멈춤");
-		speed = 0;
+	void stop() {
+		System.out.println("[자동차가 멈춥니다.]");
 	}
 }
---
+++ Child2.java
@@ -0,0 +1,9 @@
+package sec07.exam02;
+
+public class Child2 extends parent2 {
+ public String field2;
+ 
+ public void method3() {
+	 System.out.println("Child-method3()");
+ }
+}
