4d07ebebef2de2793e69505374809fc74bdc9b05
yunseok36 <kys010306@gmail.com>
2025-03-11 18:50:14 +0900

    Add files via upload

---
+++ DriverExample.java
@@ -0,0 +1,13 @@
+package sec07.exam02;
+
+public class DriverExample {
+	public static void main(String[] args) {
+		Driver driver = new Driver();
+		
+		Bus bus = new Bus();
+		Taxi taxi = new Taxi();
+		
+		driver.drive(bus); // 자동타입변환 : Vehicle vehicle = bus;
+		driver.drive(taxi); // 자동타입변환 : Vehicle vehicle = taxi;
+	}
+}
