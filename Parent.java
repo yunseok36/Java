16766a9fb36aa80cd6c9f5249abcf83de0024ac0
yunseok36 <kys010306@gmail.com>
2025-03-14 14:27:01 +0900

    Add files via upload

---
+++ Parent.java
@@ -0,0 +1,11 @@
+package sec07.exam02;
+
+public class Parent {
+	public void method1() { // method1() 선언
+		System.out.println("Parent-method1()");
+	}
+	
+	public void method2() { // method2() 선언
+		System.out.println("Parent-method2()");
+	}
+}
