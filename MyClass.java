bdbc0470f239d9b0a26947a28bf4927ad0b5a8db
ｙｕｎｓｅｏｋ <kys010306@gmail.com>
2025-03-29 00:02:42 +0900

    Add files via upload

---
+++ MyClass.java
@@ -0,0 +1,29 @@
+package sec08.exam01;
+
+public class MyClass {
+
+	// 필드
+		RemoteControl rc = new Television();
+		
+		// 생성자
+		MyClass() {
+		}
+		
+		MyClass(RemoteControl rc) {
+			this.rc = rc;
+			rc.turnOn();
+			rc.setVolume(5);
+		}
+		
+		// 메소드
+		void methodA() {
+			RemoteControl rc = new Audio();
+			rc.turnOn();
+			rc.setVolume(5);
+		}
+		
+		void methodB(RemoteControl rc) {
+			rc.turnOn();
+			rc.setVolume(5);
+		}
+}
---
+++ RemoteControlExample.java
@@ -0,0 +1,9 @@
+package sec08.exam01;
+
+public class RemoteControlExample {
+	public static void main(String[] args) {
+		RemoteControl rc;
+		rc = new Television();
+		rc = new Audio();
+	}
+}
