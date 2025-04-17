785bbf6df4d80c6ffa03543baa96aa8151b1210b
ｙｕｎｓｅｏｋ <kys010306@gmail.com>
2025-04-17 11:29:34 +0900

    Add files via upload

--- Taxi.java
+++ Taxi.java
@@ -1,8 +1,8 @@
-package sec07.exam02;
+package sec08.exam02;
 
-public class Taxi extends Vehicle { // 자식클래스
- @Override
- public void run() {
-	 System.out.println("택시가 달립니다.");
- }
+public class Taxi implements Vehicle {
+	@Override
+	public void run() {
+		System.out.println("택시가 달립니다.");
+	}
 }
--- Tire.java
+++ Tire.java
@@ -1,28 +1,5 @@
-package sec07.exam02;
+package sec08.exam02;
 
-public class Tire {
-
-	// 필드 생성
-	public int maxRotation; // 최대 회전수(타이어 수명)
-	public int accumulatedRotation; // 누적 회전수
-	public String location; // 타이어의 위치
-	
-	// 생성자
-	public Tire(String location, int maxRotation) {
-		this.location = location;
-		this.maxRotation = maxRotation;
-	}
-	
-	// 메소드
-	public boolean roll() {
-		++accumulatedRotation; // 누적 회전수 1 증가
-		if(accumulatedRotation < maxRotation) {
-			System.out.println(location + " Tire 수명 :" +
-				(maxRotation - accumulatedRotation) + "회");
-			return true;
-		} else {
-			System.out.println("*** " + location + " Tire 펑크 ***");
-			return false;
-		}
-	}
+public interface Tire {
+	public void roll(); // roll() 메소드 호출 방법 설명
 }
