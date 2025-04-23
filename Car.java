0bff03affbbe87000976c2f62cb5f115cb07d7d9
ｙｕｎｓｅｏｋ <kys010306@gmail.com>
2025-04-23 19:15:14 +0900

    Add files via upload

--- Car.java
+++ Car.java
@@ -1,31 +1,8 @@
-package sec06.exam02;
+package sec06.exam03;
 
 public class Car {
-	//필드 생성
-	private int speed;
-	private boolean stop;
-	
-	// 생성자
-	
-	// 메소드
-	public int getSpeed() {
-		return speed;
-	}
-	public void setSpeed(int speed) {
-		if(speed < 0) {
-			this.speed = 0;
-			return;
-		} else {
-			this.speed = speed;
-		}
-	}
-	
-	public boolean isStop() {
-		return stop;
-	}
-	public void serStop(boolean stop) {
-		this.stop = stop;
-		this.speed = 0;
-	}
 
+	// 생성자
+	Car(String color, int cc) {
+	}
 }
--- Car2.java
+++ Car2.java
@@ -1,8 +1,29 @@
-package sec06.exam01;
+package sec06.exam03;
 
 public class Car2 {
 
-	Car2(String color, int cc) {
-		
+	// 필드 생성
+	String company = "현대자동차";
+	String model;
+	String color;
+	int maxSpeed;
+	
+	// 생성자
+	Car2() { // 기본 생성자
+	}
+	
+	Car2(String model) {
+		this.model = model; // 생성자
+	}
+	
+	Car2(String model, String color) {
+		this.model = model;
+		this.color = color;
+	}
+	
+	Car2(String model, String color, int maxSpeed) {
+		this.model = model;
+		this.color = color;
+		this.maxSpeed = maxSpeed;
 	}
 }
