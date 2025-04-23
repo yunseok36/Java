e158d27687b7d44ffc629dcc918a6dd9f11b2852
ｙｕｎｓｅｏｋ <kys010306@gmail.com>
2025-04-24 00:09:13 +0900

    Add files via upload

--- Car2Example.java
+++ Car2Example.java
@@ -1,8 +1,26 @@
-package sec06.exam01;
+package sec06.exam03;
 
 public class Car2Example {
 	public static void main(String[] args) {
-		Car2 myCar = new Car2("검정", 3000); // 객체를 사용하기 위해 객체 Car2의 메모리 주소값을 myCar에 저장
-		// Car2 myCar = new Car2( ); (x) -> 기본 생성자를 호출할 수 없다.
+		Car2 car1 = new Car2();
+		System.out.println("car1.company : " + car1.company);
+		System.out.println();
+		
+		Car2 car2 = new Car2("자가용");
+		System.out.println("car2.company : " + car2.company);
+		System.out.println("car2.model : " + car2.model);
+		System.out.println();
+		
+		Car2 car3 = new Car2("자가용", "빨강");
+		System.out.println("car3.company : " + car3.company);
+		System.out.println("car3.model : " + car3.model);
+		System.out.println("car3.color : " + car3.color);
+		System.out.println();
+		
+		Car2 car4 = new Car2("택시", "검정", 200);
+		System.out.println("car4.company : " + car4.company);
+		System.out.println("car4.model : " + car4.model);
+		System.out.println("car4.color : " + car4.color);
+		System.out.println("car4.maxSpeed : " + car4.maxSpeed);
 	}
 }
--- Car3.java
+++ Car3.java
@@ -1,26 +1,26 @@
-package sec06.exam01;
+package sec06.exam03;
 
 public class Car3 {
-	
-	// 필드
-	String company = "BMW";
+
+	// 필드 생성
+	String company = "현대자동차";
 	String model;
 	String color;
 	int maxSpeed;
 	
 	// 생성자
-	Car3() {
+	Car3(){
 	}
-	Car3(String model) {
-		this.model = model;
+	
+	Car3(String model) { 
+		this(model, "은색", 250);
 	}
 	
 	Car3(String model, String color) {
-		this.model = model;
-		this.color = color;
+		this(model, color, 250);
 	}
 	
-	Car3(String model, String color, int maxSpeed) {
+	Car3(String model, String color, int maxSpeed) { // 공통 실행코드
 		this.model = model;
 		this.color = color;
 		this.maxSpeed = maxSpeed;
