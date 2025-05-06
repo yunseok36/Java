c2fde044cee1494e25c2bd419a9170f60cab761a
ｙｕｎｓｅｏｋ <kys010306@gmail.com>
2025-05-06 15:55:30 +0900

    Add files via upload

--- Car2Example.java
+++ Car2Example.java
@@ -1,26 +1,11 @@
-package sec06.exam03;
+package sec06.exam04;
 
 public class Car2Example {
 	public static void main(String[] args) {
-		Car2 car1 = new Car2();
-		System.out.println("car1.company : " + car1.company);
-		System.out.println();
-		
-		Car2 car2 = new Car2("자가용");
-		System.out.println("car2.company : " + car2.company);
-		System.out.println("car2.model : " + car2.model);
-		System.out.println();
-		
-		Car2 car3 = new Car2("자가용", "빨강");
-		System.out.println("car3.company : " + car3.company);
-		System.out.println("car3.model : " + car3.model);
-		System.out.println("car3.color : " + car3.color);
-		System.out.println();
-		
-		Car2 car4 = new Car2("택시", "검정", 200);
-		System.out.println("car4.company : " + car4.company);
-		System.out.println("car4.model : " + car4.model);
-		System.out.println("car4.color : " + car4.color);
-		System.out.println("car4.maxSpeed : " + car4.maxSpeed);
+		Car2 myCar = new Car2();
+		myCar.keyTurnOn();
+		myCar.run();
+		int speed = myCar.getSpeed();
+		System.out.println("현재속도 : " + speed + "km/h)");
 	}
 }
--- CarExample.java
+++ CarExample.java
@@ -1,8 +1,21 @@
-package sec06.exam03;
+package sec06.exam04;
 
 public class CarExample {
 	public static void main(String[] args) {
-		Car myCar = new Car("검정", 3000);
-		// Car myCar = new Car(); ->  기본 생성자를 호출할 수 없다
+		Car myCar = new Car();
+		
+		myCar.setGas(5); // Car의 setGas() 메소드를 호출
+		
+		boolean gasState = myCar.isLeftGas(); // Car의 isLeftGas() 메소드 호출
+		if(gasState) {
+			System.out.println("출발합니다.");
+			myCar.run(); // Car의 run() 메소드 호출
+		}
+		
+		if(myCar.isLeftGas()) {
+			System.out.println("gas를 주입할 필요가 없습니다.");
+		} else {
+			System.out.println("gas를 주입하세요");
+		}
 	}
 }
