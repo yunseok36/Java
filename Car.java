ad834c99d92e8391f7e2b8374c06d4b47d04969f
ｙｕｎｓｅｏｋ <kys010306@gmail.com>
2025-05-09 21:17:12 +0900

    Add files via upload

--- Car.java
+++ Car.java
@@ -1,35 +1,25 @@
-package sec06.exam04;
+package sec06.exam05;
 
 public class Car {
 
 	// 필드 선언
-	int gas; // 필드 gas에 값이 지정되어 있지 않기 때문에, 초기값 0으로 할당된다.
+	String model;
+	int speed;
 	
-	// 생성자
-	
-	// 메소드
-	void setGas(int gas) {
-		this.gas = gas; // 리턴값이 없는 메소드로 매개값을 받아서 gas 필드값을 변경
+	// 생성자 선언
+	Car(String model) {
+		this.model = model;
 	}
 	
-	boolean isLeftGas() {
-		if(gas == 0) {
-			System.out.println("gas가 없습니다.");
-			return false; // false를 리턴
-		}
-		System.out.println("gas가 있습니다.");
-		return true; // true를 리턴
+	// 메소드 선언
+	void setSpeed(int speed) {
+		this.speed = speed;
 	}
 	
 	void run() {
-		while(true) {
-			if(gas > 0) {
-				System.out.println("달립니다. (gas잔량 : " + gas + ")");
-				gas -= 1; // gas = gas - 1;
-			} else {
-				System.out.println("멈춥니다. (gas잔량 : " + gas + ")");
-				return; // 메소드 실행 종료
-			}
+		for(int i  = 10; i <= 50; i += 10) {
+			this.setSpeed(i);
+			System.out.println(this.model + "가 달립니다. (시속 : " + this.speed + "km/h)");
 		}
 	}
 }
--- CarExample.java
+++ CarExample.java
@@ -1,21 +1,12 @@
-package sec06.exam04;
+package sec06.exam05;
 
 public class CarExample {
 	public static void main(String[] args) {
-		Car myCar = new Car();
+		Car myCar = new Car("포르쉐");
+		Car yourCar = new Car("벤츠");
+		// myCar와 yourCar는 다른 객체를 공유함
 		
-		myCar.setGas(5); // Car의 setGas() 메소드를 호출
-		
-		boolean gasState = myCar.isLeftGas(); // Car의 isLeftGas() 메소드 호출
-		if(gasState) {
-			System.out.println("출발합니다.");
-			myCar.run(); // Car의 run() 메소드 호출
-		}
-		
-		if(myCar.isLeftGas()) {
-			System.out.println("gas를 주입할 필요가 없습니다.");
-		} else {
-			System.out.println("gas를 주입하세요");
-		}
+		myCar.run();
+		yourCar.run();
 	}
 }
