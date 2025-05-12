35f4ab92979ffc47917d9510865925260b79849b
ｙｕｎｓｅｏｋ <kys010306@gmail.com>
2025-05-13 00:10:38 +0900

    Add files via upload

--- Car2.java
+++ Car2.java
@@ -1,25 +1,15 @@
-package sec06.exam04;
+package sec06.exam05;
 
 public class Car2 {
-
-	// 필드 생성
 	int speed;
 	
-	// 생성자
-	
-	// 메소드
-	int getSpeed() {
-		return speed;
-	}
-	
-	void keyTurnOn() {
-		System.out.println("키를 돌립니다.");
-	}
-	
 	void run() {
-		for(int i = 10; i <= 50; i+=10) {
-			speed = i;
-			System.out.println("달립니다. (시속 :" + speed + "km/h)");
-		}
+		System.out.println(speed + "으로 달립니다.");
+	}
+	
+	public static void main(String[] args) {
+		Car2 myCar = new Car2(); // static으로 객체 생성 후 사용
+		myCar.speed = 60;
+		myCar.run();
 	}
 }
