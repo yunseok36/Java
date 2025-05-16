c120d119dfe6a3895d253a6bdb4b43eba9e2d9cf
ｙｕｎｓｅｏｋ <kys010306@gmail.com>
2025-05-17 00:29:26 +0900

    Add files via upload

--- SingletonExample.java
+++ SingletonExample.java
@@ -1,14 +1,15 @@
-package sec06.exam01;
+package sec06.exam05;
 
 public class SingletonExample {
 	public static void main(String[] args) {
 		/*
-		 * Singleton obj = new Singleton(); // 컴파일 에러 발생
-		 * Singleton obj2 = new Singleton(); // 이 코드는 객체를 2개 생성함, private를 사용한 싱글톤에서는 컴파일 에러 발생
+		 Singleton obj1 = new Singleton();
+		 Singleton obj2 = new Singleton(); 
+		 -> 객체를 2개 생성하여 private를 사용한 싱글톤에서는 컴파일 에러 발생
 		 */
 		
 		Singleton obj1 = Singleton.getInstance();
-		Singleton obj2 = Singleton.getInstance();
+		Singleton obj2 = Singleton.getInstance(); // obj1, obj2 는 Singleton 객체의 메모리 주소값
 		
 		if(obj1 == obj2) {
 			System.out.println("같은 Singleton 객체입니다.");
