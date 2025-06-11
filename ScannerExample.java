f3efd0599c3a82ab9f7eb452c2a1d2e9388270d5
ｙｕｎｓｅｏｋ <kys010306@gmail.com>
2025-06-12 00:38:24 +0900

    Add files via upload

---
+++ ScannerExample.java
@@ -0,0 +1,21 @@
+package sec01.exam04;
+
+import java.util.Scanner;
+
+public class ScannerExample {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String inputData;
+		
+		while(true) {
+			inputData = scanner.nextLine();
+			System.out.println("입력된 문자열 : \"" + inputData + "\"");
+			
+			if(inputData.equals("q")) {
+				break;
+			}
+		}
+		
+		System.out.println("종료");
+	}
+}
