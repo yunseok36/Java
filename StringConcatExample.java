65b2fa1fe8312f8f201262ca634f23135f21fe92
ｙｕｎｓｅｏｋ <kys010306@gmail.com>
2025-06-22 22:22:01 +0900

    Add files via upload
    Strimg Concat Example file for java

--- StringConcatExample.java
+++ StringConcatExample.java
@@ -1,14 +1,22 @@
-package sec02.exam01;
+package sec01.exam03;
 
 public class StringConcatExample {
 	public static void main(String[] args) {
-		String str1 = "JDK" + 6.0;
-		String str2 = str1 + " 특징";
-		System.out.println(str2); // JDK6.0 특징, 문자열 연결연산자가 먼저 실행되기 때문에 모두 문자열로 바뀌어서 연산 결과 출력 됌
+		// 숫자 연산
+		int value = 10 + 2+ 8;
+		System.out.println("value : " + value); // 20
 		
-		String str3 = "JDK" + 3 + 3.0;
-		String str4 = 3 + 3.0 + "JDK";
-		System.out.println(str3); // JDK33.0, 문자열 연결연산자가 먼저 실행되기 때문에 모두 문자열로 바뀌어서 연산 결과 출력 됌
-		System.out.println(str4); // 6.0JDK, 산술 연산이 먼저 실행되고 문자열 연결
+		// 문자열 변환
+		String str1 = 10 + 2 + "8";
+		System.out.println("str1 : " + str1); // 숫자 X, 128
+		
+		String str2 = 10 + "2" + 8;
+		System.out.println("str2 : " + str2); // 숫자 X, 1028
+		
+		String str3 = "10" + 2 + 8;
+		System.out.println("str3 : " + str3); // 숫자 X, 1028
+		
+		String str4 = "10" + (2 + 8);
+		System.out.println("str4 : " + str4); // 숫자 X, 1010
 	}
 }
